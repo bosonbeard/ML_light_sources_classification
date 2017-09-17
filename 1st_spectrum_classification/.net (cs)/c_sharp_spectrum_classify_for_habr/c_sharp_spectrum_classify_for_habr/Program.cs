348dc6ebac272df6d7a4a2c0b5ce1ded6226be91
bosonbeard <bosonbeard@gmail.com>
2017-09-17 22:18:07 +0300

    changed folder
    changed structure of project

---
+++ 1st_spectrum_classification/.net (cs)/c_sharp_spectrum_classify_for_habr/c_sharp_spectrum_classify_for_habr/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using Accord.Statistics.Models.Regression.Linear;
+using Accord.Statistics.Analysis;
+using Accord.IO;
+using Accord.Math;
+using System.Data;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics.Kernels;
+using Accord.Controls;
+
+namespace c_sharp_spectrum_classify_for_habr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for correct symbol of float point
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            //This is a program for demonstrating machine 
+            //learning and classifying the spectrum of light sources using .net
+
+
+            //read data (If you use linux do not forget to correct the path to the files)
+            string trainCsvFilePath = @"data\train.csv";
+            string testCsvFilePath = @"data\test.csv";
+            DataTable trainTable = new CsvReader(trainCsvFilePath, true).ToTable();
+            DataTable testTable = new CsvReader(testCsvFilePath, true).ToTable();
+ 
+
+            // Convert the DataTable to input and output vectors (train and test)
+            int[] trainOutputs = trainTable.Columns["label"].ToArray<int>();
+                        trainTable.Columns.Remove("label");
+            double[][] trainInputs = trainTable.ToJagged<double>();
+            int[] testOutputs = testTable.Columns["label"].ToArray<int>();
+            testTable.Columns.Remove("label");
+            double[][] testInputs = testTable.ToJagged<double>();
+
+            // training  model SVM classifier
+            var teacher = new MulticlassSupportVectorLearning<Gaussian>()
+            {
+                // Configure the learning algorithm to use SMO to train the
+                //  underlying SVMs in each of the binary class subproblems.
+                Learner = (param) => new SequentialMinimalOptimization<Gaussian>()
+                {
+                    // Estimate a suitable guess for the Gaussian kernel's parameters.
+                    // This estimate can serve as a starting point for a grid search.
+                    UseKernelEstimation = true
+                }
+            };
+
+            // Learn a machine
+            var machine = teacher.Learn(trainInputs, trainOutputs);
+
+            // Obtain class predictions for each sample
+            int[] predicted = machine.Decide(testInputs);
+
+            // print result
+            int i = 0;
+            Console.WriteLine("results - (predict ,real labels)");
+            foreach (int pred in predicted)
+            {
+
+                Console.Write("({0},{1} )", pred, testOutputs[i]);
+                i++;
+            }
+
+            //calculate the accuracy
+            double error = new ZeroOneLoss(testOutputs).Loss(predicted);
+
+            Console.WriteLine("\n accuracy: {0}", 1 - error);
+
+            // consider the decrease in the dimension of features using PCA
+            var pca = new PrincipalComponentAnalysis()
+            {
+                Method = PrincipalComponentMethod.Center,
+                Whiten = true
+            };
+
+            pca.NumberOfOutputs = 2;
+            MultivariateLinearRegression transform = pca.Learn(trainInputs);
+            double[][] outputPCA = pca.Transform(trainInputs);
+
+            // print it on the scatter plot
+            ScatterplotBox.Show(outputPCA, trainOutputs).Hold();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1st_spectrum_classification/README.md
@@ -0,0 +1,60 @@
+## ML_ spectrum_classification
+
+Данный репозиторий является дополнительным материалом к статье на портале Habrahabr – https://habrahabr.ru/post/337040/
+демонстрирующим примеры классификации спектров излучения источников искусственного и естественного света представлены 
+  
+Внимание! Изложенные ниже методы и данные на основании, которых проводится классификация, собраны на основании материалов, размещенных пользователями в общем доступе на портале https://spectralworkbench.org/  и не предполагают научной достоверности.
+Учитывая возможные ошибки вызванные техническими и «человеческими» факторами рекомендуется применение набора данных исключительно в демонстрационных целях. 
+
+
+Material on the machine classification of the emission spectra of lamps and sun.
+Complements Article - https://habrahabr.ru/post/337040/
+Materials may contain errors, not recommended for serious research.
+Spectrum source https://spectralworkbench.org/  
+P.S. English text from google translate :)
+
+## Описание данных (Data description)
+Набор данных состоит из тестовой и обучающей выборки, описывающей спектры источников света.  
+Спектральные характеристики разбиты на 301 признак где значение признака соответствует длине волны в спектре.    
+Значение признака – условная мощность излучения.   
+Поле label содержит метку источника света:  
+0-	Светодиодная лампа (белый свет от WW/CW/NW/RGB светодиодов или их комбинации);  
+1-	Люминесцентная лампа (компактная или трубчатая);  
+2-	Дневной свет (солнце, небо, или их комбинация);  
+train.csv – обучающая выборка;  
+test.csv – контрольная выборка;  
+train_names csv , test_names.csv– имена источников спектра для train и test соответственно.
+Просмотреть исходный спектр можно по адресу :
+https://spectralworkbench.org/spectrums/XXXX  
+где: XXXX- имя спектра в файле.
+Например: https://spectralworkbench.org/spectrums/9740
+
+
+The data set consists of a test and training sample that describes the spectrum of light sources.
+Spectral characteristics are divided into 301 features where the value of the characteristic corresponds to the wavelength in the spectrum.
+The value of the characteristic is the emission power.  
+The label field contains the light source label:  
+0- LED lamp (white light from WW / CW / NW / RGB LEDs or their combination);  
+1- Fluorescent lamp (compact or tube);  
+2- Daylight (sun, sky, or or their combination);  
+train.csv - training sample, test.csv - testing sample;  
+train names csv, test_names.csv - filenames of the spectrum sources for train and test, respectively.
+View the original spectrum can be found at:
+https://spectralworkbench.org/spectrums/XXXX  
+where: XXXX is the name of the spectrum in the file.
+For example: https://spectralworkbench.org/spectrums/9740
+
+
+## Пример машинного обучения (Example of machine learning)
+
+Базовые модели машинного обучения представлены в файле spectrum_classify_for_habrahabr.ipynb
+
+
+Base models are presented in the  spectrum_classify_for_habrahabr.ipynb
+
+
+Если вы предпочитаете .net в папке ".net (cs)", есть небольшая демонстрация на языке C# 
+(однако, она менее информативна чем версия на python)
+
+ If you prefer .net in the ".net (cs)" folder, there is a small demonstration in C # 
+ however, it is less informative than the python version)
